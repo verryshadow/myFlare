e0707204eb936a625529cf46193cb253527868e2
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2020-10-01 14:02:49 +0200

    Fix minor bug in the mapping

--- run_flask.py
+++ run_flask.py
@@ -4,7 +4,8 @@ from run import run
 app = Flask(__name__)
 
 
-@app.route("/i2b2/", methods=["POST"])
+@app.route("/i2b2", methods=["POST"])
 def handle_i2b2_query():
     i2b2_query = request.data.decode("UTF-8")
-    run(i2b2_query)
+    result_set = run(i2b2_query)
+    return str(result_set)
