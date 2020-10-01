20aeee1b41c761a3a361ece2ad61929f8fa42dd4
Lukas Szimtenings <lszimtenings@ukaachen.de>
2020-10-01 13:38:37 +0200

    Add flask service

---
+++ run_flask.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from run import run
+
+app = Flask(__name__)
+
+
+@app.route("/i2b2/", methods=["POST"])
+def handle_i2b2_query():
+    i2b2_query = request.data.decode("UTF-8")
+    run(i2b2_query)
