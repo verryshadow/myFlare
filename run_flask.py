557b74e808959dc70d6e75e5b79235e0c564e68d
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2020-10-08 13:56:14 +0200

    Fix issue with the response

--- run_flask.py
+++ run_flask.py
@@ -1,7 +1,7 @@
+import time
 from flask import Flask, request
 import xml.etree.ElementTree as ET
 from run import run
-import timeit
 
 app = Flask(__name__)
 
@@ -9,21 +9,21 @@ app = Flask(__name__)
 @app.route("/i2b2", methods=["POST"])
 def handle_i2b2_query():
     # Execute and timestamp
-    start_time = timeit.default_timer()
+    start_time = time.time()
     i2b2_query = request.data.decode("UTF-8")
     result_set = run(i2b2_query, xml=True)
-    end_time = timeit.default_timer()
+    end_time = time.time()
     delta = end_time - start_time
 
     # Insert timestamps into result_set
     x_start_time = ET.Element("start_time")
-    x_start_time.attrib["value"] = start_time
+    x_start_time.attrib["value"] = str(start_time)
 
     x_end_time = ET.Element("end_time")
-    x_end_time.attrib["value"] = end_time
+    x_end_time.attrib["value"] = str(end_time)
 
     x_delta = ET.Element("delta")
-    x_delta.attrib["value"] = delta
+    x_delta.attrib["value"] = str(delta)
 
     result_set.insert(0, x_end_time)
     result_set.insert(0, x_start_time)
