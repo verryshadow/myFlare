89de93c743600a0aca963befad17616204b292a2
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-01-20 08:45:12 +0100

    Clean up

--- IBM_transfer/common.py
+++
@@ -1,7 +0,0 @@
-endpoints = {"Patient": [str(i) for i in range(1000, 1030)],
-             "Observation": [f'M-{i}' for i in range(1000, 1172)] + [f'O-{i}' for i in range(1000, 1622)],
-             "Encounter": [str(i) for i in range(1000, 1036)]}
-
-
-def get_temp_file_path(obj_id: str, obj_type) -> str:
-    return f'temp/{obj_type}/{obj_id}.xml'
